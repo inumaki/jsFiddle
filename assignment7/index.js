1fe49b241a4ad899c5b0f20e5e23da1328e98ef0
aryan <etoyora@gmail.com>
2023-01-12 01:10:51 +0530

    assignment7

--- assignment7/index.js
+++ assignment7/index.js
@@ -1,32 +1,71 @@
-var lists = document.getElementById("lists");
-let names = new Set();
+let numbers = new Set();
 
-function addName() {
-  let name = document.getElementById("nameInput").value;
-  name = name.split(' ');
-
-
-
-
-
-  var str = "";
-  var n = name.length;
-  for (var i = 0; i < n - 1; i++) {
-    if(name[i]=='')
-    continue;
-    
-    str += name[i][0] + " ";
-    console.log(name[i][0]);
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
   }
-  str += name[n - 1];
 
-  names.add(str);
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+////////////////////////////////////////////////////////////////////////
+function generateNumbers() {
+  numbers.clear();
+  let num = document.getElementById("numInput").value;
+  if (Number(num) < 1 || Number(num) > 10000) alert("Please Enter In Range");
+  var i = 0;
+  while (i < num) {
+    var value = Math.floor(Math.random() * 10000) + 1;
+    console.log(value);
+    numbers.add(value);
+    i = numbers.size;
+  }
+  let rNumbers = document.getElementById("random");
+
+  rNumbers.innerHTML = "";
+  for (let nums of numbers) rNumbers.innerHTML += "<p>" + nums + "</p>";
+}
+ 
+//........................................................................................
+
+
+function listPrimes() {
+  if (numbers.size <= 0) {
+    alert("Enter Number Properly");
+    return;
+  }
+
+  let primeList = document.getElementById("primeList");
+  primeList.innerHTML = "";
+  let primes = new Set();
+  let primeCount = 0;
+  let minPrime = Number.MAX_SAFE_INTEGER;
+  let maxPrime = Number.MIN_SAFE_INTEGER;
+
+  for (let number of numbers) {
+    if (isPrime(number)) {
+      primes.add(number);
+      primeCount++;
+      minPrime = Math.min(minPrime, number);
+      maxPrime = Math.max(maxPrime, number);
+    }
+  }
+
+  console.log(primeList);
+  for (let prime of primes) {
+    primeList.innerHTML += "<div>" + prime + "</div>";
+  }
+  if (primeCount === 0) {
+    minPrime = "NONE";
+    maxPrime = "NONE";
+  }
+
+  primeList.innerHTML += `<p> Total prime numbers : ${primeCount}</p>`;
+  primeList.innerHTML += `<p> Max prime number : ${maxPrime}</p>`;
+  primeList.innerHTML += `<p> Min prime number : ${minPrime}</p>`;
 }
 
-function displayNames() {
-  let nameList = document.getElementById("lists");
-  nameList.innerHTML = "";
-  for (let name of names) {
-    nameList.innerHTML += "<li>" + name + "</li>";
-  }
-}
+
+//........................................................................................
